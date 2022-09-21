0f439c9b2e14c55c1d15bb6d1dce763b938042ec
Gustavo Gonçalves <guga1507@hotmail.com>
2022-09-21 01:53:10 -0300

    Refatoracao Questao 4

--- Questao4/Program.cs
+++ Questao4/Program.cs
@@ -2,18 +2,41 @@
 {
     public static void Main()
     {
-        double
-            SP = 67836.43, //37,52
-            RJ = 36678.66,
-            MG = 29229.88,
-            ES = 27165.48,
-            Outros = 19849.53,
-            total = SP + RJ + MG + ES + Outros;
+        List<Estado> estados;
 
-        Console.WriteLine("Faturamento no estado de SP: " + (100 / total) * SP);
-        Console.WriteLine("Faturamento no estado do RJ: " + (100 / total) * RJ);
-        Console.WriteLine("Faturamento no estado de MG: " + (100 / total) * MG);
-        Console.WriteLine("Faturamento no estado do ES: " + (100 / total) * ES);
-        Console.WriteLine("Faturamento nos Outros estados: " + (100 / total) * Outros);
+        estados = new List<Estado>();
+        estados.Add(new Estado("SP", 67836.43));
+        estados.Add(new Estado("RJ", 36678.66));
+        estados.Add(new Estado("MG", 29229.88));
+        estados.Add(new Estado("ES", 27165.48));
+        estados.Add(new Estado("Outros", 19849.53));
+
+        double total = 0;
+
+        // Soma dos valores por estado, para obter o faturamento total
+        foreach (Estado est in estados)
+        {
+            total = total + est.Valor;
+        }
+
+        Console.WriteLine("Percentual de faturamento por estado:\n");
+
+        // Calculo e Exibição dos percentuais de cada estado
+        foreach (Estado est in estados)
+        {
+            Console.WriteLine(est.Nome + ": " + Math.Round((100 / total) * est.Valor, 2) + "%");
+        }
+    }
+
+    public class Estado
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+        public Estado(string nome, double valor)
+        {
+            this.Nome = nome;
+            this.Valor = valor;
+        }
     }
 }
